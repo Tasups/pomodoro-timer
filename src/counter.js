97bc78cc5a4c4a6e9d73c63d911b38702e9e5869
EC2 Default User <ec2-user@ip-172-31-24-64.us-east-2.compute.internal>
2022-01-31 15:20:30 +0000

    Counter core logic implemented
    Signed-off-by: EC2 Default User <ec2-user@ip-172-31-24-64.us-east-2.compute.internal>

--- src/counter.js
+++ src/counter.js
@@ -2,25 +2,46 @@ import React, { useState } from 'react';
 
 const Counter = () => {
   
-  const [time, setTime] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const handleMinutesChange = (e) => {
+    setMinutes(e.target.value);
+  }
   
-  const second = 1000;
-  const minute = second * 60;
+  const handleSecondsChange = (e) => {
+    setSeconds(e.target.value);
+  }
   
-  let timeValue;
-  
-  const handleSetTime = (timeValue) => {
+  const handleSetTime = (e) => {
+    e.preventDefault();
   }
   
   return (
     <React.Fragment>
       <form type="submit">
         <label for="minutes">minutes</label>
-        <input type="number" name="minutes" id="minutes" max="60"/>
+        <input 
+          type="number" 
+          name="minutes" 
+          id="minutes" 
+          max="60" 
+          value={minutes} 
+          onChange={handleMinutesChange}
+        />
+        {minutes}
         <label for="seconds">seconds</label>
-        <input type="number" name="seconds" id="seconds" max="60"/>
+        <input 
+          type="number" 
+          name="seconds" 
+          id="seconds" 
+          max="60" 
+          value={seconds}
+          onChange={handleSecondsChange}  
+        />
+        {seconds}
       </form>
-      <button type="submit">Start</button>
+      <button type="submit" onClick={handleSetTime}>Start</button>
     </React.Fragment>
     )
 }
